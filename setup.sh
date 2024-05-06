c9921d5b308a4e0a5bcb3eec08e080425a448c81
zhavens <zhavens@hotmail.com>
2024-05-06 14:43:37 -0500

    Adding automated vim plugin mgmt

--- setup.sh
+++ setup.sh
@@ -3,3 +3,7 @@
 for f in $(ls -a | egrep "^\.\w+"); do
     cp $f ~/
 done
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim --clean '+source ~/.vimrc' +PluginInstall +qall
